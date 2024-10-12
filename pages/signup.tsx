251e7c0e14c8e4db2fbe655b7d308cefbb8a2b3e
Karan <karan.modi9@gmail.com>
2024-10-12 16:01:20 -0400

    Prepare for GitHub Pages deployment

---
+++ pages/signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      {/* Add your signup form or content here */}
+    </div>
+  );
+};
+
+export default Signup;
