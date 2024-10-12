2b8451d13a2285723d48820b20faba3bc06149eb
Karan <karan.modi9@gmail.com>
2024-10-12 11:01:58 -0400

    Deploying images and other assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Uncomment and set the basePath if your project is not at the root of the domain
+  // basePath: '/CareCentral',
+};
 
 export default nextConfig;
